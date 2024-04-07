f199d7f5f782eb9beb7e8e60b063cb722cf5c4f0
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-04-07 22:19:04 +0530

    styling done and server actions set up is also done

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,4 +1,19 @@
+import { prisma } from "@/db";
 import Link from "next/link";
+import { redirect } from "next/navigation";
+
+// Server action
+async function createTodo(data: FormData){
+    // to run on the server
+    "use server"
+    const title = data.get("title")?.valueOf;
+    if(typeof title !== "string" || title.length === 0){
+        throw new Error("Invalid Title");
+    }
+
+    await prisma.todo.create({data: {title, completed: false}});
+    redirect("/");
+}
 
 export default function Page(){
     return (
@@ -6,7 +21,7 @@ export default function Page(){
             <header className="flex justify-between items-center mb-4">
                 <h1 className="text-2xl">New</h1>
             </header>
-            <form action="" className="flex gap-2 flex-col">
+            <form action={createTodo} className="flex gap-2 flex-col">
                 <input type="text" placeholder="create a new todo" name="title" className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100" />
                 <div className="flex justify-end gap-1">
                     <Link href=".." className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within: border-slate-100">Cancel</Link>
