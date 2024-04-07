927396565ad4901fecf399801e55c60d03464fb7
Aditya Ghosh <adi27.ghosh@gmail.com>
2024-04-07 20:07:07 +0530

    design of new todo page is completed

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,5 +1,18 @@
+import Link from "next/link";
+
 export default function Page(){
     return (
-        <h1>New</h1>
+        <>
+            <header className="flex justify-between items-center mb-4">
+                <h1 className="text-2xl">New</h1>
+            </header>
+            <form action="" className="flex gap-2 flex-col">
+                <input type="text" placeholder="create a new todo" name="title" className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100" />
+                <div className="flex justify-end gap-1">
+                    <Link href=".." className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within: border-slate-100">Cancel</Link>
+                    <button className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within: border-slate-100">Create</button>
+                </div>
+            </form>
+        </>
     )
 }
